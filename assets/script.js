7777f2cf3bc95c18ea10d9bf17eed5644f396e10
Ben Currier <benjamin.currier1@gmail.com>
2023-06-05 14:29:19 -0500

    latest and greatest troubleshooting with console logs

--- assets/script.js
+++ assets/script.js
@@ -1,31 +1,7 @@
-// // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// // the code isn't run until the browser has finished rendering all the elements
-// // in the html.
-// $(function () {
-//   // TODO: Add a listener for click events on the save button. This code should
-//   // use the id in the containing time-block as a key to save the user input in
-//   // local storage. HINT: What does `this` reference in the click listener
-//   // function? How can DOM traversal be used to get the "hour-x" id of the
-//   // time-block containing the button that was clicked? How might the id be
-//   // useful when saving the description in local storage?
-//   //
-//   // TODO: Add code to apply the past, present, or future class to each time
-//   // block by comparing the id to the current hour. HINTS: How can the id
-//   // attribute of each time-block be used to conditionally add or remove the
-//   // past, present, and future classes? How can Day.js be used to get the
-//   // current hour in 24-hour time?
-//   //
-//   // TODO: Add code to get any user input that was saved in localStorage and set
-//   // the values of the corresponding textarea elements. HINT: How can the id
-//   // attribute of each time-block be used to do this?
-//   //
-//   // TODO: Add code to display the current date in the header of the page.
-// });
-
 // One fuction to create an individual card for each hour for current day/hour
 
 
-$(function () {
+$(document).ready(function () {
   // Display the current date in the header of the page
   var currentDate = dayjs().format("dddd, MMMM D, YYYY");
   $("#currentDay").text(currentDate);
@@ -41,18 +17,28 @@ $(function () {
   });
 
   // Apply the past, present, future class to each time block
-  var currentHour = dayjs().format("H");
-  $(".time-block").each(function () {
-    var timeBlockHour = parseInt($(this).attr("id").split("-")[1]);
+  function timeOfDate() {
 
-    if (timeBlockHour < currentHour) {
-      $(this).addClass("past");
-    } else if (timeBlockHour === currentHour) {
-      $(this).addClass("present");
-    } else {
-      $(this).addClass("future");
-    }
-  });
+    var currentHour = dayjs().format("H");
+    // wrap in a function and call it on row 44
+    $(".time-block").each(function () {
+      var timeBlockHour = parseInt($(this).attr("id").split("-")[1]);
+  
+      if (timeBlockHour < currentHour) {
+        $(this).addClass("past");
+      } else if (timeBlockHour === currentHour) {
+        $(this).removeClass("past");
+        $(this).addClass("present");
+      } else {
+        $(this).removeClass("past");
+        $(this).removeClass("present");
+        $(this).addClass("future");
+  
+      }
+    });
+  }
+
+  timeOfDate();
 
   // Get user input from local storage and set textarea values
   $(".time-block").each(function () {
